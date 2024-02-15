48a6f047e07c4a0d369e963b9ab1c35fdd311d2a
ThaiBinh1608 <zuesgod1608@gmail.com>
2024-02-15 12:10:42 +0700

    update

--- controllers/insertData.js
+++ controllers/insertData.js
@@ -2,8 +2,8 @@ const { response } = require("express");
 const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
-const data = require("../../data/ecommerce.json");
-const categorydata = require("../../data/cateBrand");
+const data = require("../data/ecommerce.json");
+const categorydata = require("../data/cateBrand");
 const ProductCategory = require("../../server/models/productCategory");
 
 const fn = async (product) => {
---
+++ data/cateBrand.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    cate: "SmartPhone",
+    brand: ["Apple", "samsung", "LG", "Asus", "BlackBerry", "Nokia"],
+    image:
+      "https://ibixion.com/wp-content/uploads/2018/02/best-android-phones.jpg",
+  },
+  {
+    cate: "Speaker",
+    brand: ["Sony", "samsung"],
+    image:
+      "https://i5.walmartimages.com/asr/4ed7293b-440b-4c69-9662-d3cbc0ace678_1.b61eb782676c8bf47963adf5c15bffc9.jpeg",
+  },
+
+  {
+    cate: "Camera",
+    brand: ["LG", "Sony"],
+    image:
+      "https://www.bhphotovideo.com/images/images2500x2500/panasonic_dmc_zs60_s_lumix_dmc_zs60_digital_camera_1213208.jpg",
+  },
+
+  {
+    cate: "Tablet",
+    brand: ["Ipad", "Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+    image:
+      "https://digital-world-2.myshopify.com/cdn/shop/files/pc-1_300x.jpg?v=1613166682",
+  },
+  {
+    cate: "Laptop",
+    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Macbook"],
+    image:
+      "https://digital-world-2.myshopify.com/cdn/shop/files/laptop_300x.jpg?v=1613166811",
+  },
+  {
+    cate: "Accessories",
+    brand: [
+      "Smartswatches",
+      "Headphone",
+      "Bluetooth",
+      "Keyboard",
+      "Mouse",
+      "Cases",
+    ],
+    image:
+      "https://digital-world-2.myshopify.com/cdn/shop/files/headphone_8467ba67-70c7-4977-b57e-8847d56549c6_300x.jpg?v=1613166811",
+  },
+  {
+    cate: "Television",
+    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+    image:
+      "https://digital-world-2.myshopify.com/cdn/shop/files/television_300x.jpg?v=1613166810",
+  },
+  {
+    cate: "Printer",
+    brand: ["Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+    image:
+      "https://digital-world-2.myshopify.com/cdn/shop/files/printer_300x.jpg?v=1613166810",
+  },
+];
+module.exports = data;
